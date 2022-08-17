a5c65216fa1c48f8a925f57008a8b459a8ecc8ea
MirzaAyon <ayonmirza98@gmail.com>
2022-08-17 10:07:10 +0600

    navbar added

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from '../Link/Link';
+import { MenuIcon, XIcon } from '@heroicons/react/solid'
 
 const Navbar = () => {
+    const [open, setOpen] = useState(false)
+    //open thakle close hbe and close thakle pen hbe 
     const routes = [
         { id: 1, name: 'Home', link: '/home' },
         { id: 2, name: 'Shop', link: '/shop' },
@@ -10,8 +13,13 @@ const Navbar = () => {
         { id: 4, name: 'contact', link: '/contact' },
     ]
     return (
-        <nav>
-            <ul>
+        <nav className='bg-indigo-200'>
+            <div onClick={() => setOpen(!open)} className='w-6 h-6 md:hidden'>
+                {open ? <XIcon></XIcon> : <MenuIcon></MenuIcon>}
+
+            </div>
+
+            <ul className={`md:flex justify-center md:static absolute bg-indigo-200 w-full duration-500 ease-in ${open ? 'top-6' : 'top-[-120px]'}`}>
                 {/* <li><a href=""></a></li>
                 <li><a href=""></a></li>
                 <li><a href=""></a></li>
---
+++ src/Components/Pricing/Pricing.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Pricing = () => {
+    return (
+        <div>
+            <h1 className='text-6xl'>Best deal of the town</h1>
+        </div>
+    );
+};
+
+export default Pricing;
