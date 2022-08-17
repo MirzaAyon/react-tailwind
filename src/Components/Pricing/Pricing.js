bff78f66ce77a5450123687f82eb46d70037e0a3
MirzaAyon <ayonmirza98@gmail.com>
2022-08-17 11:56:37 +0600

    responsive pricing added using react and tailwind

--- src/Components/Pricing/Pricing.js
+++ src/Components/Pricing/Pricing.js
@@ -3,15 +3,33 @@ import PricingOption from '../PricingOption/PricingOption';
 
 const Pricing = () => {
     const pricingOptions = [
-        { id: 1, name: 'free', price: 0 },
-        { id: 2, name: 'regular', price: 9.99 },
-        { id: 3, name: 'premium', price: 19.99 },
+        {
+            id: 1, name: 'free', price: 0, benefits: ['lifetime free',
+                'unlimited deals',
+                'localized deals',
+                'fantastic deals',
+                'crazy deals']
+        },
+        {
+            id: 2, name: 'regular', price: 9.99, benefits: ['everything on free',
+                'unlimited deals',
+                'localized deals',
+                'fantastic deals',
+                'crazy deals']
+        },
+        {
+            id: 3, name: 'premium', price: 19.99, benefits: ['everything on free',
+                'unlimited deals',
+                'localized deals',
+                'fantastic deals',
+                'crazy deals']
+        },
     ]
     return (
         <div className='bg-indigo-300 p-4 mt-8'>
             <h1 className='text-6xl font-mono text-white'>Best deal of the town</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum commodi fugit quam officiis, iusto quibusdam a itaque blanditiis voluptatum minima!</p>
-            <div className='grid md:grid-cols-3 gap-3'>
+            <div className='grid md:grid-cols-3 gap-3 mt-8'>
                 {
                     pricingOptions.map(option => <PricingOption
                         key={option.id}
